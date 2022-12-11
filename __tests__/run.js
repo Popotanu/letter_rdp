2b16bb7ad30634d7b3d999e86e24cb6fc629aa55
popotanu <scrapmacaron@gmail.com>
2022-12-11 17:15:33 +0900

    feat: tokenize comments

--- __tests__/run.js
+++ __tests__/run.js
@@ -4,14 +4,27 @@ const { Parser } = require("../src/parser");
 
 const parser = new Parser();
 
-const programNum = `  42  `;
+const programNum = `  " 42 "  `;
 const programStr = `"tanu12"`;
 const programStr2 = `'tanu12'`;
+const programComment = `
+
+// Number:
+42
+
+/**
+ * tanutanu;
+ */
+
+
+`;
 
 const astNum = parser.parse(programNum);
 const astStr = parser.parse(programStr);
 const astStr2 = parser.parse(programStr2);
+const astComment = parser.parse(programComment);
 
 console.log(JSON.stringify(astNum, null, 2));
 console.log(JSON.stringify(astStr, null, 2));
 console.log(JSON.stringify(astStr2, null, 2));
+console.log(JSON.stringify(astComment, null, 2));
