4c9303516a3b710927d136b6572161ea203a8b5b
popotanu <scrapmacaron@gmail.com>
2022-12-11 19:13:15 +0900

    test: use assert

---
+++ __tests__/literals-test.js
@@ -0,0 +1,26 @@
+module.exports = (test) => {
+  // NumericLiteral
+  test(`42`, {
+    type: "Program",
+    body: {
+      type: "NumericLiteral",
+      value: 42,
+    },
+  });
+
+  test(`"hello"`, {
+    type: "Program",
+    body: {
+      type: "StringLiteral",
+      value: "hello",
+    },
+  });
+
+  test(`'hello'`, {
+    type: "Program",
+    body: {
+      type: "StringLiteral",
+      value: "hello",
+    },
+  });
+};
