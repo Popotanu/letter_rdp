49098344e47bea1baa20a4bd804e47dcc938991c
popotanu <scrapmacaron@gmail.com>
2022-12-14 19:35:17 +0900

    test: add variable test

--- __tests__/run.js
+++ __tests__/run.js
@@ -13,6 +13,7 @@ const tests = [
   require("./empty-statement"),
   require("./math-test.js"),
   require("./assignment-test.js"),
+  require("./variable-test.js"),
 ];
 
 const parser = new Parser();
@@ -31,6 +32,7 @@ function exec() {
 
   // Number:
   x = y = 32;
+  let x =  32;
 
   `;
 
